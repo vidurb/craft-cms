f80a1487b9ad0cc6648594b8bce90de197f10784
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-07-22 13:33:40 +0100

    add craft support webpack config

---
+++ src/web/assets/craftsupport/webpack.config.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+/* globals module, require */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('webpack-merge');
+const decache = require('decache');
+decache('../../../../webpack.base.asset.config');
+const BASE_CONFIG = require('../../../../webpack.base.asset.config');
+
+module.exports = merge(BASE_CONFIG,{
+    entry: ['./CraftSupportWidget.js'],
+    output: {
+        filename: 'CraftSupportWidget.min.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './logos',
+                to: './logos',
+            }]
+        }),
+    ]
+});
