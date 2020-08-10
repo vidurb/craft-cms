3603e346ccdf97cf9974326d3b5789716d194b32
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-10 11:09:19 +0100

    Update admintable asset bundle

--- src/web/assets/admintable/webpack.config.js
+++ src/web/assets/admintable/webpack.config.js
@@ -4,8 +4,6 @@ const merge = require('webpack-merge');
 const decache = require('decache');
 decache('../../../../webpack.base.vue.config');
 const BASE_CONFIG = require('../../../../webpack.base.vue.config');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(BASE_CONFIG, {
     entry: { app: './main.js'},
