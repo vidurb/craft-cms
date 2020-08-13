6a7f14994668401521f1de5393b95ea9d23bdc70
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-13 16:44:26 +0100

    Added routes asset webpack config

---
+++ src/web/assets/routes/webpack.config.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6 */
+/* globals module, require */
+const merge = require('webpack-merge');
+const decache = require('decache');
+decache('../../../../webpack.base.asset.config');
+const BASE_CONFIG = require('../../../../webpack.base.asset.config');
+
+module.exports = merge(BASE_CONFIG,{
+    entry: {'routes': './routes.js'},
+});
