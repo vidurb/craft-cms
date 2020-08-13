61dcc0d081e312561e0b4854cd3d7b3bd715347a
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-13 16:00:04 +0100

    build installer asset files

--- src/web/assets/installer/webpack.config.js
+++ src/web/assets/installer/webpack.config.js
@@ -6,5 +6,5 @@ decache('../../../../webpack.base.asset.config');
 const BASE_CONFIG = require('../../../../webpack.base.asset.config');
 
 module.exports = merge(BASE_CONFIG,{
-    entry: {'transforms': './transforms.js'},
+    entry: {'install': './install.js'},
 });
