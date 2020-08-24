1b924b6c38723207ba35c9d4c0cb0388ba0693f0
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-24 15:26:49 +0100

    Updated axios asset buld

---
+++ src/web/assets/axios/webpack.config.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+/* globals module, require */
+const merge = require('webpack-merge');
+const decache = require('decache');
+decache('../../../../webpack.base.asset.config');
+const BASE_CONFIG = require('../../../../webpack.base.asset.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NODE_MODULES = __dirname + '/../../../../node_modules/';
+
+module.exports = merge(BASE_CONFIG, {
+    entry: {'entry': './entry.js'},
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    context: NODE_MODULES + '/axios/dist',
+                    from: 'axios.min.*',
+                },
+            ],
+        }),
+    ]
+});
--- webpack.libs.config.js
+++ webpack.libs.config.js
@@ -5,29 +5,30 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 const BASE_PATH = __dirname;
-const LIB_PATH = BASE_PATH + '/lib';
+const LIB_PATH = BASE_PATH + '/lib/js';
 const DEFAULT_CONFIG = require(path.join(__dirname, 'webpack.base.config'));
 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const JSONMinifyPlugin = require('node-json-minify');
 
 let jsLibs = {
-    'jquery-touch-events': '@benmajor/jquery-touch-events/src/jquery.mobile-events.js',
-    'fileupload': 'blueimp-file-upload/js/jquery.fileupload.js',
-    'd3': 'd3/build/d3.js',
-    'element-resize-detector': 'element-resize-detector/dist/element-resize-detector.js',
+    'd3': 'd3/build/d3.min.js',
+    'element-resize-detector': 'element-resize-detector/dist/element-resize-detector.min.js',
     'fabric': 'fabric/dist/fabric.js',
-    'garnishjs': 'garnishjs/dist/garnish.js',
-    'inputmask': 'inputmask/dist/jquery.inputmask.bundle.js',
-    'jquery': 'jquery/dist/jquery.js',
-    'jquery.payment': 'jquery.payment/lib/jquery.payment.js',
-    'iframe-resizer': 'iframe-resizer/js/iframeResizer.js',
-    'iframe-resizer-cw': 'iframe-resizer/js/iframeResizer.contentWindow.js',
-    'picturefill': 'picturefill/dist/picturefill.js',
+    'fileupload': 'blueimp-file-upload/js/jquery.fileupload.js',
+    'garnishjs': 'garnishjs/dist/garnish.min.js',
+    'iframe-resizer': 'iframe-resizer/js/iframeResizer.min.js',
+    'iframe-resizer-cw': 'iframe-resizer/js/iframeResizer.contentWindow.min.js',
+    'inputmask': 'inputmask/dist/min/jquery.inputmask.bundle.min.js',
+    'jquery': 'jquery/dist/jquery.min.js',
+    'jquery-touch-events': '@benmajor/jquery-touch-events/src/jquery.mobile-events.min.js',
+    'jquery.payment': 'jquery.payment/lib/jquery.payment.min.js',
+    'picturefill': 'picturefill/dist/picturefill.min.js',
     'punycode': 'punycode/punycode.js',
-    'selectize': 'selectize/dist/js/standalone/selectize.js',
-    'timepicker': 'timepicker/jquery.timepicker.js',
-    'velocity': 'velocity-animate/velocity.js',
+    'selectize': 'selectize/dist/js/standalone/selectize.min.js',
+    'timepicker': 'timepicker/jquery.timepicker.min.js',
+    'velocity': 'velocity-animate/velocity.min.js',
     'xregexp': 'xregexp/xregexp-all.js',
     'yii2-pjax': 'yii2-pjax/jquery.pjax.js',
 };
@@ -37,20 +38,25 @@ let copyFiles = [
     'd3-time-format/locale/*.json',
 ];
 
-module.exports = merge(DEFAULT_CONFIG, {
-    context: BASE_PATH + '/node_modules',
-    entry: jsLibs,
+module.exports = merge({}, {
+    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    context: BASE_PATH + '/lib',
+    entry: './libs.js',
     output: {
         filename: '[name]/[name].js',
         path: LIB_PATH,
     },
+    optimization: {
+        minimize: false,
+    },
     plugins: [
+        new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: copyFiles.map(row => {
                 if (typeof row === 'string') {
                     return {
-                        from: row,
-                        to: '.',
+                        from: BASE_PATH + '/node_modules/' + row,
+                        to: LIB_PATH + '/.',
                         transform: function(content) {
                             return JSONMinifyPlugin(content.toString());
                         }
