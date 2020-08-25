b0144a2c5a55c27324a776302749d4c0497179eb
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-25 10:59:02 +0100

    Updated velocity asset

---
+++ src/web/assets/velocity/webpack.config.js
@@ -0,0 +1,23 @@
+/* jshint esversion: 6 */
+/* globals module, require, __dirname */
+const merge = require('webpack-merge');
+const decache = require('decache');
+decache('../../../../webpack.base.asset.config');
+const BASE_CONFIG = require('../../../../webpack.base.asset.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NODE_MODULES = __dirname + '/../../../../node_modules/';
+
+module.exports = merge(BASE_CONFIG, {
+    entry: {'entry': './entry.js'},
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    context: NODE_MODULES + 'velocity-animate',
+                    from: 'velocity.min.js',
+                    to: 'velocity.js'
+                },
+            ]
+        })
+    ]
+});
