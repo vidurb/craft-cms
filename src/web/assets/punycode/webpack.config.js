1c67104b60ac84413e8fa594dd10aa504c1b2470
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-08-26 15:43:33 +0100

    Added punycode asset

---
+++ src/web/assets/punycode/webpack.config.js
@@ -0,0 +1,21 @@
+/* jshint esversion: 6 */
+/* globals module, require, __dirname */
+const merge = require('webpack-merge');
+const decache = require('decache');
+decache('../../../../webpack.base.asset.config');
+const BASE_CONFIG = require('../../../../webpack.base.asset.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(BASE_CONFIG, {
+    entry: {'entry': './entry.js'},
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './punycode.*',
+                    to: '.'
+                }
+            ]
+        }),
+    ]
+});
